c3408ea658d57618e5b6a500f62d30df4da5d51c
huttami <hhuttami@gmail.com>
2023-04-05 15:10:45 -0700

    change env file

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
@@ -12,6 +13,6 @@ class Settings:
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}" 
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:%s@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}" % quote_plus(POSTGRES_PASSWORD)
 
 settings = Settings()    
